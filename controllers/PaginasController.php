c6245ea815a38c638e0bacfe41db2d35ec699fa2
rvaldez20 <rvaldez20@gmail.com>
2024-07-04 23:28:16 -0600

    [ADD] create routes for pages public zone

---
+++ controllers/PaginasController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Controllers;
+
+class PaginasController {
+
+   public static function index() {
+      echo 'desde index';
+   }
+
+   public static function nosotros() {
+      echo 'desde nosotros';
+   }
+
+   public static function propiedades() {
+      echo 'desde propiedades';
+   }
+
+   public static function propiedad() {
+      echo 'desde propiedad';
+   }
+
+   public static function blog() {
+      echo 'desde blog';
+   }
+
+   public static function entrada() {
+      echo 'desde entrada';
+   }
+
+   public static function contacto() {
+      echo 'desde contacto';
+   }
+}
--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\PaginasController;
 use MVC\Router;
 use Controllers\PropiedadController;
 use Controllers\VendedorController;
@@ -11,6 +12,7 @@ $router = new Router();
 
 //debug(PropiedadController::class);  // retorna namespace: Controller\PropiedadController
 
+//? Zona Pivada o Protegida
 $router->get('/admin', [PropiedadController::class, 'index']);
 $router->get('/propiedades/crear', [PropiedadController::class, 'crear']);
 $router->post('/propiedades/crear', [PropiedadController::class, 'crear']);
@@ -18,14 +20,21 @@ $router->get('/propiedades/actualizar', [PropiedadController::class, 'actualizar
 $router->post('/propiedades/actualizar', [PropiedadController::class, 'actualizar']);
 $router->post('/propiedades/eliminar', [PropiedadController::class, 'eliminar']);
 
-
 $router->get('/vendedores/crear', [VendedorController::class, 'crear']);
 $router->post('/vendedores/crear', [VendedorController::class, 'crear']);
 $router->get('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
 $router->post('/vendedores/actualizar', [VendedorController::class, 'actualizar']);
 $router->post('/vendedores/eliminar', [VendedorController::class, 'eliminar']);
 
-
+//? Zona Publica
+$router->get('/', [PaginasController::class, 'index']);
+$router->get('/nosotros', [PaginasController::class, 'nosotros']);
+$router->get('/propiedades', [PaginasController::class, 'propiedades']);
+$router->get('/propiedad', [PaginasController::class, 'propiedad']);
+$router->get('/blog', [PaginasController::class, 'blog']);
+$router->get('/entrada', [PaginasController::class, 'entrada']);
+$router->get('/contacto', [PaginasController::class, 'contacto']);
+$router->post('/contacto', [PaginasController::class, 'contacto']);
 
 
 $router->comprobarRutas();
